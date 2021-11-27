97704f3199c74399132401578f8051b0bd2a5164
samko.s <ssamko091184@gmail.com>
2021-11-27 08:58:43 +0200

    HW7 taskADD done

---
+++ src/HWSevenAnonAndEnum/TaskAdditional/Run.java
@@ -0,0 +1,11 @@
+package HWSevenAnonAndEnum.TaskAdditional;
+
+public class Run {
+
+    public static void main(String[] args) {
+        Vehicles vehicleOne = Vehicles.CYCLE;
+        System.out.println(vehicleOne);
+        Vehicles vehicleTwo = Vehicles.CAR;
+        System.out.println(vehicleTwo);
+    }
+}
